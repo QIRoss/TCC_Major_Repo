5151773c73b685cae3e2df7780bce564dfcf3e53
qiross <lucasqreis@poli.ufrj.br>
2024-08-19 20:12:32 -0300

    Removing comments

--- stop_all_except_jenkins.sh
+++ stop_all_except_jenkins.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Get the ID of the container named "my_jenkins"
 jenkins_id=$(docker ps -q -f name=my_jenkins)
 
-# Stop all containers except "my_jenkins"
 docker ps -q | while read -r container_id; do
     if [ "$container_id" != "$jenkins_id" ]; then
         docker stop "$container_id"
